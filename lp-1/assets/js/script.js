5453b80d31944c0c38e8d5b71775e0f0b902a14b
YUKI NAGAHAMA <yukinagahama@YUKIs-MacBook-Pro.local>
2024-07-17 11:19:25 +0900

    電話番号のバリデーションも追加2

--- lp-1/assets/js/script.js
+++ lp-1/assets/js/script.js
@@ -597,7 +597,7 @@ async function completeStep() {
       age_vali.style.display = "none";
       age_vali.classList.remove("shake");
 
-      const phoneRegex = /^(0[5-9]0[-(]?[0-9]{4}[-)]?[0-9]{4}|0120[-]?\d{1,3}[-]?\d{4}|050[-]?\d{4}[-]?\d{4}|0[1-9][-]?\d{1,4}[-]?\d{1,4}[-]?\d{4})*$/;
+      const phoneRegex = /^(0[5-9]0[-(]?[0-9]{4}[-)]?[0-9]{4}|0120[-]?\d{1,3}[-]?\d{4}|050[-]?\d{4}[-]?\d{4}|0\d{1,3}[-]?\d{1,4}[-]?\d{4}|0570[-]?\d{1,4}[-]?\d{4})$/;
       if (phone_number === "" || !phoneRegex.test(phone_number)) {
         phone_vali.style.display = "block";
         phone_vali.classList.add("shake");
