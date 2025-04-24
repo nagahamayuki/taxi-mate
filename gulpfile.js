0aadb33dc4838e7ff9c01e4c2c6f1bc8036e0855
Yuki Nagahama <yuuki4104453@gmail.com>
2025-04-24 13:26:31 +0900

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,76 @@
-const gulp = require('gulp');
-const uglify = require('gulp-uglify');
-const cleanCSS = require('gulp-clean-css');
-const sourcemaps = require('gulp-sourcemaps');
-const concat = require('gulp-concat');
+const gulp = require("gulp");
+const uglify = require("gulp-uglify");
+const cleanCSS = require("gulp-clean-css");
+const sourcemaps = require("gulp-sourcemaps");
+const concat = require("gulp-concat");
 
-// JSファイルを圧縮するタスク
+// lp-1用のJS圧縮
 function scriptsLp1() {
-    return gulp.src('lp-1/assets/js/script.js') // JSファイルのソースディレクトリ
-        .pipe(sourcemaps.init())
-        .pipe(concat('script.min.js'))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('lp-1/assets/js')); // 圧縮後のファイルの出力ディレクトリ
+  return gulp
+    .src("lp-1/assets/js/script.js")
+    .pipe(sourcemaps.init())
+    .pipe(concat("script.min.js"))
+    .pipe(uglify())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("lp-1/assets/js"));
 }
 
-// CSSファイルを圧縮するタスク
+// lp-1用のCSS圧縮
 function stylesLp1() {
-    return gulp.src('lp-1/assets/css/style.css') // CSSファイルのソースディレクトリ
-        .pipe(sourcemaps.init())
-        .pipe(concat('style.min.css'))
-        .pipe(cleanCSS())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('lp-1/assets/css')); // 圧縮後のファイルの出力ディレクトリ
+  return gulp
+    .src("lp-1/assets/css/style.css")
+    .pipe(sourcemaps.init())
+    .pipe(concat("style.min.css"))
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("lp-1/assets/css"));
 }
 
-// 変更を監視するタスク
+// lp-2用のJS圧縮
+function scriptsLp2() {
+  return gulp
+    .src("lp-2/assets/js/script.js")
+    .pipe(sourcemaps.init())
+    .pipe(concat("script.min.js"))
+    .pipe(uglify())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("lp-2/assets/js"));
+}
+
+// lp-2用のCSS圧縮
+function stylesLp2() {
+  return gulp
+    .src("lp-2/assets/css/style.css")
+    .pipe(sourcemaps.init())
+    .pipe(concat("style.min.css"))
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("lp-2/assets/css"));
+}
+
+// 変更監視
 function watch() {
-    gulp.watch('lp-1/assets/js/*.js', scriptsLp1);
-    gulp.watch('lp-1/assets/css/*.css', stylesLp1);
+  gulp.watch(["lp-1/assets/js/*.js", "!lp-1/assets/js/*.min.js"], scriptsLp1);
+  gulp.watch(
+    ["lp-1/assets/css/*.css", "!lp-1/assets/css/*.min.css"],
+    stylesLp1
+  );
+  gulp.watch(["lp-2/assets/js/*.js", "!lp-2/assets/js/*.min.js"], scriptsLp2);
+  gulp.watch(
+    ["lp-2/assets/css/*.css", "!lp-2/assets/css/*.min.css"],
+    stylesLp2
+  );
 }
 
-// デプロイ専用のタスク
-const deploy = gulp.series(scriptsLp1, stylesLp1);
+// デプロイ用一括実行
+const deploy = gulp.series(scriptsLp1, stylesLp1, scriptsLp2, stylesLp2);
+const defaultTask = gulp.series(deploy, watch);
 
-// デフォルトタスク
-const defaultTask = gulp.series(scriptsLp1, stylesLp1, watch);
-
-// タスクのエクスポート
+// エクスポート
 exports.scriptsLp1 = scriptsLp1;
 exports.stylesLp1 = stylesLp1;
+exports.scriptsLp2 = scriptsLp2;
+exports.stylesLp2 = stylesLp2;
 exports.watch = watch;
 exports.deploy = deploy;
 exports.default = defaultTask;
